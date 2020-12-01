c352affa72727b1acd5f959206845adf25bfa21c
john.maguire <john.maguire@cbinsights.com>
2020-12-01 10:23:04 -0500

    Added challenge1 for 2020

---
+++ 2020/Challenge1/puzzle1.go
@@ -0,0 +1,79 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"log"
+	"os"
+	"strconv"
+)
+
+const year int = 2020
+
+func twoProductFromTwoSum(inputs []int, matcher int) (int, int, int) {
+	diffMap := make(map[int]bool)
+	var (
+		diff    int
+		other   int
+		product int
+	)
+
+	for _, rowNum := range inputs {
+		diff = matcher - rowNum
+		if _, ok := diffMap[diff]; ok {
+			other = rowNum
+			product = diff * rowNum
+			break
+		} else {
+			diffMap[rowNum] = true
+		}
+	}
+	return product, diff, other
+}
+
+func threeProductFromThreeSum(inputs []int) int {
+	var (
+		diff    int
+		product int
+	)
+	for _, rowNum := range inputs {
+		diff = year - rowNum
+		twoProd, one, two := twoProductFromTwoSum(inputs, diff)
+		if twoProd != 0 {
+			product = rowNum * one * two
+		}
+	}
+	return product
+}
+
+func main() {
+	file, err := os.Open("input.txt")
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	defer file.Close()
+
+	scanner := bufio.NewScanner(file)
+	rowNums := make([]int, 0)
+	for scanner.Scan() {
+		rowNum, err := strconv.Atoi(scanner.Text())
+		if err != nil {
+			log.Fatal(err)
+		}
+		rowNums = append(rowNums, rowNum)
+	}
+	twoProduct, _, _ := twoProductFromTwoSum(rowNums, year)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	fmt.Printf("Two Product: %d\n", twoProduct)
+
+	threeProduct := threeProductFromThreeSum(rowNums)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	fmt.Printf("Three Product: %d\n", threeProduct)
+}
