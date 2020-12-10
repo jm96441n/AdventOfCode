6cacc4145fe92d45bd85a75538d3aa321034c12d
john.maguire <john.maguire@cbinsights.com>
2020-12-10 15:57:17 -0500

    Finished challenge 10

--- file_utils/file.go
+++ file_utils/file.go
@@ -4,6 +4,7 @@ import (
 	"bufio"
 	"log"
 	"os"
+	"strconv"
 )
 
 func OpenFileIntoSlice(fileName string) []string {
@@ -18,6 +19,29 @@ func OpenFileIntoSlice(fileName string) []string {
 	rows := make([]string, 0)
 	for scanner.Scan() {
 		rows = append(rows, scanner.Text())
+	}
+	if err := scanner.Err(); err != nil {
+		log.Fatal(err)
+	}
+	return rows
+}
+
+func OpenFileIntoIntSlice(fileName string) []int {
+	file, err := os.Open(fileName)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	defer file.Close()
+	scanner := bufio.NewScanner(file)
+
+	rows := make([]int, 0)
+	for scanner.Scan() {
+		intRow, err := strconv.Atoi(scanner.Text())
+		if err != nil {
+			log.Fatal(err)
+		}
+		rows = append(rows, intRow)
 	}
 	if err := scanner.Err(); err != nil {
 		log.Fatal(err)
