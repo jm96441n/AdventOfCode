97d454633b9cec75df5b516ea675c49c296363af
jm96441n <john@jmaguire.tech>
2020-12-12 14:39:19 -0500

    Challenge12 pt1 done

--- 2020/main.go
+++ 2020/main.go
@@ -11,7 +11,8 @@ import (
 	//challenge "AdventOfCode/2020/challenge8"
 	//challenge "AdventOfCode/2020/challenge9"
 	//challenge "AdventOfCode/2020/challenge10"
-	challenge "AdventOfCode/2020/challenge11"
+	//	challenge "AdventOfCode/2020/challenge11"
+	challenge "AdventOfCode/2020/challenge12"
 )
 
 func main() {
