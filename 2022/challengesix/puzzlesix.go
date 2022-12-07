247a6f820dee29d0fb26310aad929b377c14e7e6
jm96441n <john@jmaguire.tech>
2022-12-06 20:53:48 -0500

    Changed six based on convo

--- 2022/challengesix/puzzlesix.go
+++ 2022/challengesix/puzzlesix.go
@@ -32,6 +32,27 @@ func idxOfRun(input string, subStrLen int) int {
 			continue
 		}
 		vals[ch] += 1
+		if len(vals) == subStrLen {
+			return idx + 1
+		}
+		vals[rune(input[idx-(subStrLen-1)])] -= 1
+		if vals[rune(input[idx-(subStrLen-1)])] == 0 {
+			delete(vals, rune(input[idx-(subStrLen-1)]))
+		}
+
+	}
+	return -1
+}
+
+// changed this function to the one above after discussion on gophers slack
+func idxOfRunOriginal(input string, subStrLen int) int {
+	vals := make(map[rune]int, 26)
+	for idx, ch := range input {
+		if idx < subStrLen-1 {
+			vals[ch] += 1
+			continue
+		}
+		vals[ch] += 1
 		if all(vals) {
 			return idx + 1
 		}
